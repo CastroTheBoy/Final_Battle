b9014a11b98c190db743abdae25f5324c57eff03
Otto Jānis Jansons <jansons1975@gmail.com>
2024-01-06 11:24:18 +0200

    First commit, console formatting

--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿// 
+﻿// Console formatting
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Title = "The Final Battle";
+Console.Clear();
+
+// Code
